24d3e79ea7f30edb7968415b14928dcdabad4702
JinYang0323 <jinyang0323@gmail.com>
2019-04-19 10:05:42 -0700

    final course materials update

---
+++ Course Documents/03 Process:Thread Synchronization/ThreadSyncProblem/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ThreadSyncProblem
+{
+    class Program
+    {
+        static int bankBalance = 0;
+        static object obj = new Object();
+
+        static Random rand = new Random();
+
+        static Queue<int> queue = new Queue<int>();
+
+        static void ProducerFunction()
+        {
+
+            while (true)
+            {
+
+
+                    int val = rand.Next(50);
+                    queue.Enqueue(val);
+                    Console.WriteLine(Thread.CurrentThread.Name + " Enqued: " + val);
+                    Thread.Sleep(200);
+
+            }
+
+        }
+
+        static void ConsumerFunction()
+        {
+            while (true)
+            {
+                if (queue.Count > 0)
+                {
+                    int val = queue.Dequeue();
+
+                    Console.WriteLine(Thread.CurrentThread.Name + " Dequeued: " + val);
+                    Thread.Sleep(500);
+
+                }
+
+
+            }
+
+        }
+        static void AddMoneyToBank()
+        {
+
+            while (true)
+            {
+                lock (obj)
+                {
+                    int temp = bankBalance;
+                    Thread.Sleep(500);
+                    temp = temp + 1;
+                    bankBalance = temp;
+                    Console.WriteLine("Company " + Thread.CurrentThread.Name + " added a $ current Balance = " + bankBalance);
+                    Thread.Sleep(1000);
+
+                }
+
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Thread bluetooth = new Thread(AddMoneyToBank);
+            // Thread northEastern = new Thread(AddMoneyToBank);
+            // bluetooth.Name = "Bluetooth";
+            // northEastern.Name = "North Eastern";
+
+            // bluetooth.Start();
+            // Thread.Sleep(500);
+            // northEastern.Start();
+
+            Thread producer1 = new Thread(ProducerFunction);
+            producer1.Name = "producer1";
+            Thread producer2 = new Thread(ProducerFunction);
+            producer2.Name = "producer2";
+            Thread producer3 = new Thread(ProducerFunction);
+            producer3.Name = "producer3";
+
+            Thread consumer1 = new Thread(ConsumerFunction);
+            consumer1.Name = "consumer1";
+            Thread consumer2 = new Thread(ConsumerFunction);
+            consumer2.Name = "consumer2";
+
+            producer1.Start();
+            Thread.Sleep(100);
+            producer2.Start();
+            Thread.Sleep(100);
+            producer3.Start();
+            Thread.Sleep(100);
+
+            // consumer1.Start();
+            // Thread.Sleep(100);
+            // consumer2.Start();
+            // Thread.Sleep(100);
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+            }
+
+        }
+    }
+}
